134acf5b418d67ebf7e04aad2eca669162c8e753
Michael Montero <mcmontero@gmail.com>
2014-01-12 21:29:19 -0500

    Added license and documentation.

--- conf/rdbms_prebuild/10-create-databases.sql
+++ conf/rdbms_prebuild/10-create-databases.sql
@@ -1,3 +1,31 @@
+/*
++------------------------------------------------------------+
+| LICENSE                                                    |
++------------------------------------------------------------+
+*/
+
+/**
+ * Copyright 2013 Michael C. Montero
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
++------------------------------------------------------------+
+| SOURCE SCHEMA                                              |
++------------------------------------------------------------+
+*/
+
 create database if not exists schema_differ_source;
 
      grant all privileges
@@ -17,6 +45,12 @@ create table schema_differ_source.add_table
 (
     col_c integer not null primary key
 );
+
+/*
++------------------------------------------------------------+
+| TARGET SCHEMA                                              |
++------------------------------------------------------------+
+*/
 
 create database if not exists schema_differ_target;
 
